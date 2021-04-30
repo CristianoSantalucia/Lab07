52a107d0bbb66396dcd6364fbdc31eacb4aea677
crist <crist@SURFACE-CRIS>
2021-04-30 11:43:51 +0200

    versione completa da Emanuela

--- src/main/java/it/polito/tdp/poweroutages/model/Model.java
+++ src/main/java/it/polito/tdp/poweroutages/model/Model.java
@@ -1,7 +1,9 @@
 package it.polito.tdp.poweroutages.model;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -9,7 +11,11 @@ import it.polito.tdp.poweroutages.DAO.PowerOutageDAO;
 
 public class Model
 {
-	PowerOutageDAO dao;
+	private PowerOutageDAO dao;
+	private long MAX_YEARS;
+	private long MAX_HOURS;
+	Set<Blackout> blackouts;
+	Set<Blackout> best;
 
 	public Model()
 	{
@@ -19,32 +25,84 @@ public class Model
 	public List<Nerc> getNercList()
 	{
 		return dao.getNercList();
-	}
+	} 
 	
-	/**
-	 * Genera un insieme di lista tra cui scegliere la migliore parziale (max Affected)
-	 * @param nerc
-	 * @param maxYears
-	 * @param maxHours
-	 * @return la best parziale
-	 */
+	//	
 	public void cercaSequenza(Nerc nerc, int maxYears, int maxHours)
 	{
-		Set<Blackout> blackouts = new HashSet<>();
-		for (Blackout b : dao.getBlackoutByNerc(nerc))
-			if(Duration.between(b.getInizio(), b.getFine()).toMinutes() <= maxHours*60)
-				blackouts.add(b);
+		MAX_YEARS = maxYears;
+		MAX_HOURS = maxHours;
 		
+		blackouts = new HashSet<>(dao.getBlackoutByNerc(nerc));
+		best = new HashSet<Blackout>(); 
 		Set<Blackout> parziale = new HashSet<>();
-		
-		this.cerca(parziale, blackouts);
-		
-		System.out.println(parziale.size());
-		System.out.println(parziale);
+
+//		this.cerca(parziale);
 	}
 
-	private void cerca(Set<Blackout> parziale,Set<Blackout> blackouts)
+	private void cerca(Set<Blackout> parziale, long maxYears, long maxHours)
 	{
+		long years;
+		long hours; 
 		
+		if (confronta(parziale)) //best
+			System.out.println(parziale);
+		//
+		for (Blackout b : blackouts)
+		{
+			parziale.add(b);
+			if (!parziale.contains(b) && valida(parziale))
+//				this.cerca(parziale);
+			parziale.remove(b);
+		}
+	}
+
+	private boolean confronta(Set<Blackout> parziale)
+	{
+		if (calcolaPersone(parziale) > calcolaPersone(best))
+		{
+			best = new HashSet<Blackout>(parziale);
+			return true;
+		}
+		return false;
+	}
+
+	private boolean valida(Set<Blackout> parziale)
+	{
+		return calcolaOre(parziale) < MAX_HOURS && calcolaAnni(parziale) < MAX_YEARS;
+	}
+
+	private int calcolaPersone(Set<Blackout> set)
+	{
+		int pers = 0;
+		for (Blackout b : set)
+			pers += b.getAffected();
+		System.out.println(pers);
+		return pers;
+	}
+
+	private double calcolaOre(Set<Blackout> set)
+	{
+		double oreTot = 0;
+		if (!set.isEmpty()) 
+			for (Blackout b : set)
+				oreTot += Duration.between(b.getInizio(), b.getFine()).toMinutes() / 60;
+		return oreTot;
+	}
+	private int calcolaAnni(Set<Blackout> set)
+	{
+		int annoInizio = 0;
+		int annoFine = 0;
+		if (!set.isEmpty()) 
+		{
+			for (Blackout b : set)
+			{
+				int annoI = b.getInizio().getYear(); 
+				int annoF = b.getFine().getYear(); 
+				annoInizio = annoInizio>annoI ? annoI : annoInizio;
+				annoFine = annoFine<annoF ? annoF : annoFine;
+			} 
+		}
+		return (annoFine - annoInizio);
 	}
 }
